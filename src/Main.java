7d7ea32a7d36941b41c3078fd0800856a84bff1a
mrvudai <vudai16666@gmail.com>
2023-06-27 08:19:30 +0700

    first commit
    (cherry picked from commit ef27c77ae397a277a5011ed2c6c474cceaf4b2fe)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Thầy Đại chào các thanh niên");
-        System.out.println("Em Nhân chào các thanh niên");
-        System.out.println("Nam Hà Nội đêm mưa rả rích!!!");
     }
 }
