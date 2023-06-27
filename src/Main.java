e7f233fe5b792754a411d428422752825cc6ff47
ThangNP03 <2003nguyenphucthang@gmail.com>
2023-06-27 09:50:30 +0700

    Thanwg

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Thầy Đại chào các thanh niên");
+<<<<<<< HEAD
         System.out.println("Thắng chào các thanh nin nhé");
+=======
+        System.out.println("Thầy Đại chào các thanh niên một lần nữa");
+        System.out.println("Thầy Đại chào các thanh niên lần thứ 3");
+        System.out.println(" <==================== Thầy Lâm chào các thanh niên lần thứ 3 ====================> ");
+
+>>>>>>> e3ab5ca2fb20cd380f5eacbac60570fb6195a44a
     }
 }
