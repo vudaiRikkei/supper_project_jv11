edb732eb1f24eb2c23b9ae171bdd44d895858df6
ThangNP03 <2003nguyenphucthang@gmail.com>
2023-06-27 08:32:12 +0700

    Thắng commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Thầy Đại chào các thanh niên");
+        System.out.println("Thắng chào các thanh nin nhé");
     }
 }
