6ec4e3c75350795901ddc87e8a3f3d5787abb455
Nguyencongnguyen1812 <ngannt33@wru.vn>
2023-06-27 08:38:45 +0700

    ngannt

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Thầy Đại chào các thanh niên");
-        System.out.println("Em Nhân chào các thanh niên");
+        System.out.println("Chị Ngân chào các thanh niên đồng dâm");
     }
 }
