5cd434464e9b6df9f82c9027505aef82e9944b5f
quangtuan9997 <mrt.tuan97@gmail.com>
2023-06-27 08:45:12 +0700

    thanh nien

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Thầy Đại chào các thanh niên");
         System.out.println("Em Nhân chào các thanh niên");
+        System.out.println("Quang Tuấn chào các thanh niên");
     }
 }
