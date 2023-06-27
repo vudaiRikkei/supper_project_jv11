378661d999beadf1251aec80d1ddb5678d28c5dc
Nguyencongnguyen1812 <ngannt33@wru.vn>
2023-06-27 09:36:37 +0700

    update Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Thầy Đại chào các thanh niên");
-        System.out.println("Chị Ngân chào các thanh niên đồng dâm");
+        System.out.println("Ngannt đã ở đây ");
     }
 }
