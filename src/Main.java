5295bba147ec3064c8e93790e6570c782f63ecff
minh khiet <minhkhiet@MacBook-Pro-cua-minh.local>
2023-06-27 09:56:31 +0700

    test2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Thầy Đại chào các thanh niên");
-        System.out.println("minh khiết chào thầy đaị !");
+        System.out.println("minh khiết chào thầy đại !");
+        System.out.println("minh khiết chào ae !");
     }
 }
