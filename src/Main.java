0fd6c0b4c7063cf519c2827bfd2730498b0d5781
unknown <minhquanctm@gmail.com>
2023-06-27 09:46:03 +0700

    lo

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Thầy Đại chào các thanh niên");
 
-        System.out.println("minh khiết chào thầy đaị !");
+        System.out.println("quan chào các thanh niên");
 
-
-        System.out.println("Chị Ngân chào các thanh niên đồng dâm");
-
-        System.out.println("Chuyên gia AKA.Gnuhkun xin chào các đồng dâm!!!!!");
-
+        System.out.println("llllll");
 
     }
 }
