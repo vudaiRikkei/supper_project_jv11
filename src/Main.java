2a21bdc48809fa3459563e3ef1802040b7e2854e
mrvudai <vudai16666@gmail.com>
2023-06-27 09:09:29 +0700

    chao ae lan 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Thầy Đại chào các thanh niên");
+        System.out.println("Thầy Đại chào các thanh niên lần thứ 3");
     }
 }
