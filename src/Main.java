60468b65036e1a1122ad2ef7085a52e5f5d880e3
minh khiet <minhkhiet@MacBook-Pro-cua-minh.local>
2023-06-27 11:01:51 +0700

    abc

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Thầy Đại chào các thanh niên");
         System.out.println("Ngannt đã ở đây ");
+        System.out.println("");
     }
 }
