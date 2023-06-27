bc540c093cfce27ec705174816ad2dd4daf53db4
quangtuan9997 <mrt.tuan97@gmail.com>
2023-06-27 09:43:01 +0700

    chao ae

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
         System.out.println("Thầy Đại chào các thanh niên một lần nữa");
         System.out.println("Thầy Đại chào các thanh niên lần thứ 3");
         System.out.println(" <==================== Thầy Lâm chào các thanh niên lần thứ 3 ====================> ");
-
+        System.err.println("Quang Tuấn chào ae");
     }
 }
