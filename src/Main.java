144cfc545e1b921b55e94cf39aae47a448a17b86
Tô Việt Hoàng <totranhieu@gmail.com>
2023-06-27 09:38:11 +0700

    hieuhoa

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
-public class Main {
+gi public class Main {
     public static void main(String[] args) {
         System.out.println("Thầy Đại chào các thanh niên");
         System.out.println("Thầy Đại chào các thanh niên một lần nữa");
         System.out.println("Thầy Đại chào các thanh niên lần thứ 3");
+        System.out.println("ok nhé ae");
     }
 }
