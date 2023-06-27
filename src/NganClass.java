58fb612b58719e2ad5486bcf23ffc8820a82af14
Nguyencongnguyen1812 <ngannt33@wru.vn>
2023-06-27 08:54:37 +0700

    second update

---
+++ src/NganClass.java
@@ -0,0 +1,8 @@
+public class NganClass {
+
+    private String name = "ngan2k6";
+    private int age = 17;
+    private double height = 162;
+    private double weigh = 59;
+    private String status = "hẹn hò";
+}
