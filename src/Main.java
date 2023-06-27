b64e1beb8cad012649f3f3c369bc5c0f7a3a014a
ThangNP03 <2003nguyenphucthang@gmail.com>
2023-06-27 13:49:56 +0700

    jshđh

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-
         System.out.println("tuấn anh chào các thanh niên");
         System.out.println("tuấn anh chào các thanh niên lần 2");
         System.out.println("tuấn anh chào các thanh niên lần 3");
