e2d7cfa4b2d07e113398d9a904549c88efbe92c5
hungg1906 <vanhungkfc190699@gmail.com>
2023-06-27 08:32:32 +0700

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Thầy Đại chào các thanh niên");
+        System.out.println("Chuyên gia AKA.Gnuhkun xin chào các đồng dâm!!!!!");
     }
 }
