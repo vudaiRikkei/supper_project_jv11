4254f4acd13a814dd6bdc24ebe6785ce559aca99
tuan_anh <119550348+tuananhafjdjhv@users.noreply.github.com>
2023-06-27 09:58:40 +0700

    update 3

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
         System.out.println("tuấn anh chào các thanh niên");
         System.out.println("tuấn anh chào các thanh niên lần 2");
+        System.out.println("tuấn anh chào các thanh niên lần 3");
+        System.out.println("tuấn anh chào các thanh niên lần 4");
+        System.out.println("tuấn anh chào các thanh niên lần 5");
+        System.out.println("tuấn anh chào các thanh niên lần 6");
 
     }
 }
